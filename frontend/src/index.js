124551ca6a4a50f79dac8b88a23de94715f14392
Ole Herman Schumacher Elgesem <ole@northern.tech>
2019-11-07 15:23:55 +0100

    Removed console.log()
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,8 @@ import ClipboardJS from 'clipboard';
 (() => {
   new ClipboardJS('.to_clipboard_button');
   const buttons = document.querySelectorAll(".download_card");
-  console.log(buttons);
   for (let button of buttons)
   {
-      console.log(button);
       button.addEventListener("click", (e) => {
           document.querySelector("#copy_container")
           const packageName = e.target.getAttribute("data");
